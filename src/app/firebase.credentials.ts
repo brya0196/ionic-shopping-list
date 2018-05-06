a668260632b2888662fab1f441c74265737d5bcf
Bryan Astacio <bryanastacio@MacBook-Pro-de-Bryan.local>
2018-05-05 22:30:45 -0400

    added the firebase instance to the app

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCQ8QNmFFaq7pwPzydmIfGGtBrWt9fjERA",
+    authDomain: "shoopinglist-7d6bd.firebaseapp.com",
+    databaseURL: "https://shoopinglist-7d6bd.firebaseio.com",
+    projectId: "shoopinglist-7d6bd",
+    storageBucket: "",
+    messagingSenderId: "469767227933"
+  };
