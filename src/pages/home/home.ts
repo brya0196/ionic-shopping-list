45fb86877766a3e90078a96305571cf14d8e3d16
Bryan Astacio <bryanastacio@MacBook-Pro-de-Bryan.local>
2018-05-06 13:25:31 -0400

    reading items from the firebase db

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from '@firebase/util';
+import { Item } from '../../models/item/item.model';
 
 @IonicPage()
 @Component({
@@ -8,8 +11,17 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppinglist$: Observable<Item[]>;
 
+  constructor(public navCtrl: NavController, private shopping: ShoppingListService) {
+    this.shoppinglist$ = this.shopping
+      .getShoppinglist() // db list
+      .snapshotChanges() //key and values
+      .map(changes => {
+      return changes.map(c => ({
+        key: c.payload.key, ...c.payload.val()
+      }))
+    })
   }
 
 }
